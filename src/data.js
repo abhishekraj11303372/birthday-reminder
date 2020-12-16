2ce1902fce750e66b49f5f82063a50ad23d9f0ba
abhishekraj11303372 <43966764+abhishekraj11303372@users.noreply.github.com>
2020-12-17 01:37:23 +0530

    mini project complete

---
+++ src/data.js
@@ -0,0 +1,24 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+    {
+        id: 1,
+        name: 'Gal Gadot',
+        age: 35,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/5/5b/Gal_Gadot_cropped_lighting_corrected_2b.jpg',
+        
+    },
+    {
+        id: 2,
+        name: 'Johnny Depp',
+        age: 57,
+        image: 'https://www.gstatic.com/tv/thumb/persons/33623/33623_v9_bc.jpg',
+        
+    },
+    {
+        id: 3,
+        name: 'Keanu Reeves',
+        age: 56,
+        image: 'https://upload.wikimedia.org/wikipedia/commons/f/f2/Keanu_Reeves_2013_%2810615146086%29_%28cropped%29.jpg',
+        
+    },  
+]
